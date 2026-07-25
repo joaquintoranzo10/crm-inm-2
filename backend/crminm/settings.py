6acc6e141e2d07113a7d49a3caeeb368612e441f
Sebastian <msebastian552@gmail.com>
2026-07-24 23:24:06 -0300

    usamos sendgrid para mandar el correo

--- backend/crminm/settings.py
+++ backend/crminm/settings.py
@@ -165,15 +165,10 @@ EMAIL_BACKEND = os.environ.get(
     "EMAIL_BACKEND",
     "django.core.mail.backends.console.EmailBackend"
 )
-EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
-EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "False") == "True"
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "")
 DEFAULT_FROM_EMAIL = os.environ.get(
     "DEFAULT_FROM_EMAIL",
-    "Real Connect <noreply@crminm.local>"
+    "CRM Real Connect <noreply@crminm.local>"
 )
 
 CACHES = {
