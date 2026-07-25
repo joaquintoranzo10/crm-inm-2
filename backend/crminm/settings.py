ffb099e24163d77fe920a0a5570cad2737d9ddbb
Sebastian <msebastian552@gmail.com>
2026-07-24 22:59:10 -0300

    -

--- backend/crminm/settings.py
+++ backend/crminm/settings.py
@@ -168,6 +168,7 @@ EMAIL_BACKEND = os.environ.get(
 EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "False") == "True"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.environ.get(
@@ -196,4 +197,3 @@ STORAGES = {
     },
 }
 
-EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "False") == "True"
