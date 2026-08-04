79b2373f0d0a2f2a908349c2f3801d4db14350f7
juan <juanigacaceres24@gmail.com>
2026-08-04 18:53:53 -0300

    nada

--- backend/propiedades/serializers.py
+++ backend/propiedades/serializers.py
@@ -10,16 +10,6 @@ class PropiedadImagenSerializer(serializers.Serializer):
 
 class PropiedadSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-<<<<<<< HEAD
-    codigo = serializers.CharField(max_length=50)
-    titulo = serializers.CharField(max_length=255)
-    descripcion = serializers.CharField(required=False, allow_blank=True, allow_null=True)
-    ubicacion = serializers.CharField(max_length=255, required=False, allow_blank=True, allow_null=True)
-    tipo_de_propiedad = serializers.CharField(max_length=50, default='casa')
-    disponibilidad = serializers.CharField(max_length=50, default='disponible')
-    moneda = serializers.CharField(max_length=3, default='USD')
-    estado = serializers.CharField(max_length=50, default='disponible')
-=======
    
     codigo = serializers.CharField(max_length=50)
     titulo = serializers.CharField(max_length=255)
@@ -39,7 +29,6 @@ class PropiedadSerializer(serializers.Serializer):
     moneda = serializers.CharField(max_length=3, default='USD')
     estado = serializers.CharField(max_length=50, default='disponible')
  
->>>>>>> db1be1c5bc52e412cec8d048b99fc2190eabff7a
     precio = serializers.DecimalField(max_digits=12, decimal_places=2, required=False, allow_null=True)
     ambientes = serializers.IntegerField(required=False, allow_null=True)
     ambiente = serializers.IntegerField(required=False, allow_null=True)
@@ -61,30 +50,9 @@ class PropiedadSerializer(serializers.Serializer):
             return []
 
     def create(self, validated_data):
-<<<<<<< HEAD
-        has_ambientes = hasattr(Propiedad, 'ambientes')
-        has_ambiente = hasattr(Propiedad, 'ambiente')
-        ambientes_val = validated_data.pop('ambientes', None)
-        ambiente_val = validated_data.pop('ambiente', None)
-        val = ambientes_val if ambientes_val is not None else ambiente_val
-
-        if val is not None:
-            if has_ambientes:
-                validated_data['ambientes'] = val
-            elif has_ambiente:
-                validated_data['ambiente'] = val
-
-        if 'owner' not in validated_data:
-            request = self.context.get('request')
-            if request and hasattr(request, 'user') and request.user.is_authenticated:
-                validated_data['owner'] = request.user
-
-        return Propiedad.objects.create(**validated_data)
-=======
         validated_data.pop('owner', None) 
         user = self.context['request'].user
         return Propiedad.objects.create(owner=user, **validated_data)
->>>>>>> 8dc177ba8c3c7e8ad57258d939b9966106af79dc
 
     def update(self, instance, validated_data):
         has_ambientes = hasattr(Propiedad, 'ambientes')
